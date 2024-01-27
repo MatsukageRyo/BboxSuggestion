c9f1740e6325b7da1bbee082e95c81f200c14154
MatsukageRyo <mat.3.tun@gmail.com>
2024-01-27 13:49:20 +0000

    debugメッセージ追加

--- exec_code/exec.py
+++ exec_code/exec.py
@@ -35,6 +35,7 @@ def main(user_id:str, bucket_name:str):
     from main import main as BboxSuggestion
     from s3.s3_utils import s3_utils
     global s3
+    print(f'[DEBUG in exec.py] bucket_name: {bucket_name}')
     s3 = s3_utils(bucket_name)
 
     # check input
--- exec_code/exec_test.py
+++ exec_code/exec_test.py
@@ -3,6 +3,7 @@ def test_exec_code():
     try:
         bucket_name = 'bounding-box-suggestion-test'
         if os.path.isfile('/home/exec.py'): os.remove('/home/exec.py')
+        assert not os.path.isfile('/home/exec.py')
         cmd = ['bash', 'exec_code/mv_exec_code.sh']
         subprocess.check_output(cmd)
         assert os.path.isfile('/home/exec.py')
