138f0b42ba318848c26a82c457c51f662006c6d8
MatsukageRyo <mat.3.tun@gmail.com>
2024-01-27 14:22:39 +0000

    exec以下はgithub actionsでテストしない

--- exec_code/exec_test.py
+++ exec_code/exec_test.py
@@ -3,17 +3,14 @@ def test_exec_code():
     try:
         bucket_name = 'bounding-box-suggestion-test'
         
-        is_github_actions = os.path.isdir('/home/runner/')
-        if not is_github_actions:
-            if os.path.isfile('/home/exec.py'): os.remove('/home/exec.py')
-            assert not os.path.isfile('/home/exec.py')
-            cmd = ['bash', 'exec_code/mv_exec_code.sh']
-            subprocess.check_output(cmd)
-            assert os.path.isfile('/home/exec.py')
+        if os.path.isfile('/home/exec.py'): os.remove('/home/exec.py')
+        assert not os.path.isfile('/home/exec.py')
+        cmd = ['bash', 'exec_code/mv_exec_code.sh']
+        subprocess.check_output(cmd)
+        assert os.path.isfile('/home/exec.py')
 
-            cmd = ['python', '/home/exec.py', 'sample-id', bucket_name]
-        else:
-            cmd = ['python', 'exec_code/exec.py', 'sample-id', bucket_name]
+        cmd = ['python', '/home/exec.py', 'sample-id', bucket_name]
+
         subprocess.check_output(cmd)
     except:
         assert False
