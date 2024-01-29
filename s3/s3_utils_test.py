0206d19eed57bef6e5d4a905ee627ca7cbf5fcfb
MatsukageRyo <mat.3.tun@gmail.com>
2024-01-29 03:59:26 +0000

    s3上の特定キーの下にあるファイルをリストアップする

--- s3/s3_utils_test.py
+++ s3/s3_utils_test.py
@@ -25,6 +25,12 @@ def test_s3_utils():
     s3.copy_file(dir_name + test_file_name, dir_name + new_file_name2)
     assert s3.check_uploaded_file(new_file_name2, dir_name)
 
+    # Test for file_listup
+    file_list = s3.listup_files(dir_name)
+    assert 'test.txt' in file_list
+    assert 'test2.txt' in file_list
+    assert len(file_list) == 2
+
     # Test for delete file
     s3.del_file(test_file_name, dir_name)
     assert not s3.check_uploaded_file(test_file_name, dir_name)
@@ -41,7 +47,7 @@ def test_s3_utils():
 
     print('Success')
     os.remove(test_file_name)
-    os.remove(new_file_name2)
+    os.remove(new_file_name2)    
 
 if __name__ == "__main__":
     # Test
